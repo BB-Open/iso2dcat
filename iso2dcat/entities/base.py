ce1f00141e2c609a48108a18d35919bcbf1b3208
volker <volker.jaenisch@inqbus.de>
2022-03-11 09:13:54 +0100

    Handle dataset not in DCM

--- iso2dcat/entities/base.py
+++ iso2dcat/entities/base.py
@@ -91,7 +91,7 @@ class BaseEntity(BaseDCM):
                 self._base_uri = self.dcm.file_id_to_baseurl[self.uuid]
             except KeyError:
                 self._base_uri = 'https://datenadler.de'
-                self.logger.error('Dataset not in DCM')
+                self.logger.error('Dataset "{}" not in DCM'.format(self.uuid))
         return self._base_uri
 
     @property
