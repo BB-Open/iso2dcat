78bc6b5ea1d3bddb812dd0c10d9560950f5b0b8b
sandra <sandra.rum@inqbus.de>
2022-04-12 14:51:54 +0200

    Fixed Provenance and Rightstatement Uri Generation

--- iso2dcat/entities/provenance.py
+++ iso2dcat/entities/provenance.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from iso2dcat.entities.base import BaseEntity
 from iso2dcat.exceptions import EntityFailed
 from rdflib import URIRef, Literal
@@ -9,6 +11,10 @@ class ProvenanceStatement(BaseEntity):
 
     dcat_class = 'dct_ProvenanceStatement'
 
+    @property
+    def uri(self):
+        return self._uri
+
     def run(self):
         values = self.node.xpath(QUERY, namespaces=self.nsm.namespaces)
         if values:
@@ -17,6 +23,8 @@ class ProvenanceStatement(BaseEntity):
             self.inc('bad')
             raise EntityFailed
         statement = values[0]
+        hash_statement = hashlib.md5(str(statement).encode()).hexdigest()
+        self._uri = self.base_uri + '#' + self.dcat_class + '_' + hash_statement
         self.rdf.add((URIRef(self.uri), RDF.type, DCTERMS.ProvenanceStatement))
         for lang in self.get_languages():
             self.rdf.add((URIRef(self.uri), RDFS.label, Literal(statement, lang=lang)))
--- iso2dcat/entities/rightstatement.py
+++ iso2dcat/entities/rightstatement.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from rdflib import URIRef, Literal
 from rdflib.namespace import RDF, DCTERMS, RDFS
 
@@ -10,6 +12,10 @@ QUERY = './/gmd:resourceConstraints/*/gmd:otherConstraints/gmx:Anchor'
 class RightsStatement(BaseEntity):
     dcat_class = 'dct_RightsStatement'
 
+    @property
+    def uri(self):
+        return self._uri
+
     def run(self):
         values = self.node.xpath(QUERY, namespaces=self.nsm.namespaces)
         if values:
@@ -18,6 +24,8 @@ class RightsStatement(BaseEntity):
             self.inc('bad')
             raise EntityFailed
         statement = values[0]
+        hash_statement = hashlib.md5(str(statement).encode()).hexdigest()
+        self._uri = self.base_uri + '#' + self.dcat_class + '_' + hash_statement
         self.rdf.add((URIRef(self.uri), RDF.type, DCTERMS.RightsStatement))
         for lang in self.get_languages():
             self.rdf.add((URIRef(self.uri), RDFS.label, Literal(statement, lang=lang)))
