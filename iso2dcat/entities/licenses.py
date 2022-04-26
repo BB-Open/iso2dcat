4598a87e3b547496f1f486687121aabe67baf474
volker <volker.jaenisch@inqbus.de>
2022-04-26 12:58:07 +0200

    Added counting and error output for broken JSON

--- iso2dcat/entities/licenses.py
+++ iso2dcat/entities/licenses.py
@@ -26,13 +26,14 @@ class License(BaseEntity):
             'https://www.govdata.de/dl-de/zero-2-0' :'http://dcat-ap.de/def/licenses/dl-zero-de/2.0'
         }
         for license in license_nodes:
-            try:
-                if license.text[0] == '{' and license.text[-1] == '}':
+            if license.text[0] == '{' and license.text[-1] == '}':
+                try:
+                    license_obj = sj.loads(license.text)
+                except Exception as e:
+                    self.inc('JSON Error')
+                    self.logger.error('JSON Error in: {}'.format(self.node.fileIdentifier.getchildren()[0]))
                     license_fixed = license.text.replace('\\\\',"\\")
-                else:
-                    license_fixed = license.text
-                license_obj = sj.loads(license_fixed)
-
+                    license_obj = sj.loads(license_fixed)
                 uri_in = license_obj['url']
                 if uri_in in URI_MAPPING:
                     self.inc('Mapping')
@@ -40,17 +41,12 @@ class License(BaseEntity):
                 else:
                     self.inc('No Mapping')
                     uri_out = uri_in
-
                 licenses[DCTERMS.license] = URIRef(uri_out)
-
                 self.inc('DCAT License')
-            except Exception as e:
-                if license.text[0] == '{' and license.text[-1] == '}':
-                    self.inc('JSON Error')
-                    self.logger.error('JSON Error in: {}'.format(self.node.fileIdentifier.getchildren()[0]))
+            else:
                 for lang in langs:
                     licenses[DCATDE.licenseAttributionByText] = Literal(license.text, lang=lang)
-                self.inc('DCAT licenseAttributionByText')
+                    self.inc('DCAT licenseAttributionByText')
 
         if len(list(licenses.keys())) > 0:
             for tag, license in licenses.items():
