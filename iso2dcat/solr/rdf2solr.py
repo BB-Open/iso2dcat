d75422486f46015b17eae3c723493d729dc63838
sandra <sandra.rum@inqbus.de>
2022-08-16 09:04:34 +0200

    BugFix

--- iso2dcat/solr/rdf2solr.py
+++ iso2dcat/solr/rdf2solr.py
@@ -277,7 +277,7 @@ class RDF2SOLR(BaseDCM):
         self.logger.info('rdf datasets loaded')
         self.solr = pysolr.Solr(self.cfg.SOLR_URI, auth=('writer', 'Sas242!!'))
         self.logger.info('delete everything')
-        self.solr.delete('*:*')
+        self.solr.delete('*:*', commit=True)
         self.logger.info('writing datasets to solr')
         for key, data_set in progressbar.progressbar(data_sets.items()):
             self.solr.add(data_set)
