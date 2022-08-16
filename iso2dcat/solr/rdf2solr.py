6037c491f8d3841e078236f913d1e919c4222e54
sandra <sandra.rum@inqbus.de>
2022-08-16 09:16:05 +0200

    Fixed deleting

--- iso2dcat/solr/rdf2solr.py
+++ iso2dcat/solr/rdf2solr.py
@@ -277,8 +277,7 @@ class RDF2SOLR(BaseDCM):
         self.logger.info('rdf datasets loaded')
         self.solr = pysolr.Solr(self.cfg.SOLR_URI, auth=('writer', 'Sas242!!'))
         self.logger.info('delete everything')
-        self.solr.delete(q='*:*')
-        self.solr.commit()
+        self.solr.delete(q='*:*', commit=False)
         self.logger.info('writing datasets to solr')
         for key, data_set in progressbar.progressbar(data_sets.items()):
             self.solr.add(data_set)
