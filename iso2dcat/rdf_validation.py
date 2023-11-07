b3651f661780db3f4779cda6bfb9aa5f5d128701
sandra <sandra.rum@inqbus.de>
2023-11-07 11:14:38 +0100

    Validator Skript

--- iso2dcat/rdf_validation.py
+++ iso2dcat/rdf_validation.py
@@ -44,6 +44,22 @@ SELECT DISTINCT ?s
     }}
 """
 
+DISTRIBUTIONS_FOR_ALL_DATASET = """
+prefix bds: <http://www.bigdata.com/rdf/search#>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+prefix foaf: <http://xmlns.com/foaf/0.1/>
+prefix skos: <http://www.w3.org/2004/02/skos/core#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT DISTINCT ?s
+    WHERE {{
+        VALUES ?d {{ {values} }}
+        ?s a dcat:Distribution .
+        ?d dcat:distribution ?s .
+    }}
+"""
+
 PUBLISHER_FOR_DATASET = """
 prefix bds: <http://www.bigdata.com/rdf/search#>
 PREFIX dcat: <http://www.w3.org/ns/dcat#>
@@ -56,6 +72,22 @@ SELECT DISTINCT ?s
     WHERE {{
         ?s a foaf:Agent .
         <{dataset}> dct:publisher ?s .
+    }}
+"""
+
+PUBLISHER_FOR_ALL_DATASETS = """
+prefix bds: <http://www.bigdata.com/rdf/search#>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+prefix foaf: <http://xmlns.com/foaf/0.1/>
+prefix skos: <http://www.w3.org/2004/02/skos/core#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT DISTINCT ?s
+    WHERE {{
+        VALUES ?d {{ {values} }}
+        ?s a foaf:Agent .
+        ?d dct:publisher ?s .
     }}
 """
 
@@ -149,9 +181,102 @@ class RdfValidator(BaseDCM):
                 publisher_uri.append(s_uri)
         return publisher_uri
 
+class RdfValidator2(BaseDCM):
+
+    def __init__(self):
+        super(RdfValidator2, self).__init__()
+        self.setup_components()
+
+    def setup_components(self, args=None, env='Production', visitor=None, cfg=None):
+        # Get the configuration
+        if cfg:
+            pass
+        else:
+            register_config(env=env)
+            cfg = get_config()
+        zope.component.provideUtility(cfg, IIsoCfg)
+
+        # Setup the logging facility for this measurement ID
+        register_logger(visitor=visitor)
+
+        # Register the license mapper
+        self.lcm = register_licensemapper()
+
+        # Register the license mapper
+        self.fm = register_formatmapper()
+
+        self.logger.info('rdf2solr starting')
+        # Register the namespace manager
+        nsm = register_nsmanager()
+
+        # Register statistics
+        register_stat()
+        self.stat.init(self)
+
+        # Register RDF Database to write final results
+        db = register_db()
+
+    def run(self, db_name=None, limit=None):
+        self.logger.info('Loading rdf datasets')
+        datasets = self.get_dataset_uris(db_name)
+        if limit and limit < len(datasets):
+            datasets = datasets[:limit]
+        self.logger.info(len(datasets))
+        start = time.time()
+        self.get_publisher(db_name, datasets)
+        self.get_distributions(db_name, datasets)
+        end = time.time()
+        self.logger.info(f'Time needed {end-start}s')
+        self.logger.info('rdf datasets loaded')
+
+    def get_dataset_uris(self, db_name):
+        self.logger.info('Process Datasets')
+
+        if db_name is None:
+            db_name = self.cfg.RDF2SOLR_SOURCE
+        results = self.rdf4j.query_repository(db_name, ALL_DATASETS)
+        triples = results['results']['bindings']
+        dataset_uris = []
+        for res in triples:
+            s_uri = res['s']['value']
+            if s_uri not in dataset_uris:
+                dataset_uris.append(s_uri)
+        return dataset_uris
+
+    def get_distributions(self, db_name, datasets):
+        if db_name is None:
+            db_name = self.cfg.RDF2SOLR_SOURCE
+        values = '<' + '> <'.join(datasets) + '>'
+        results = self.rdf4j.query_repository(db_name, DISTRIBUTIONS_FOR_ALL_DATASET.format(values=values))
+        triples = results['results']['bindings']
+        dist_uris = []
+        # For all datasets collect all attributes
+        for res in triples:
+            s_uri = res['s']['value']
+            if s_uri not in dist_uris:
+                dist_uris.append(s_uri)
+        return dist_uris
+
+    def get_publisher(self, db_name, datasets):
+        if db_name is None:
+            db_name = self.cfg.RDF2SOLR_SOURCE
+        values = '<' + '> <'.join(datasets) + '>'
+        results = self.rdf4j.query_repository(db_name, PUBLISHER_FOR_ALL_DATASETS.format(values=values))
+        triples = results['results']['bindings']
+        publisher_uri = []
+        # For all datasets collect all attributes
+        for res in triples:
+            s_uri = res['s']['value']
+            if s_uri not in publisher_uri:
+                publisher_uri.append(s_uri)
+        return publisher_uri
+
+
 
 def main():
     rdf_validator = RdfValidator()
+    rdf_validator.run(limit=100)
+    rdf_validator = RdfValidator2()
     rdf_validator.run(limit=100)
 
 
